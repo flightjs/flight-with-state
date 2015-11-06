df49a386a4e90c7e453d87cb541f8d01207cc59a
Tom Ashworth <tashworth@twitter.com>
2015-11-06 13:47:39 +0000

    feat: call UMD withState

--- config/webpack.config.publish.js
+++ config/webpack.config.publish.js
@@ -3,7 +3,7 @@ var baseConfig = require('./webpack.config');
 
 module.exports = Object.assign(baseConfig, {
     output: {
-        library: 'flight-with-state',
+        library: 'withState',
         filename: 'flight-with-state.js',
         libraryTarget: 'umd',
         path: constants.BUILD_DIRECTORY
